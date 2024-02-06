f98e4dd59075e0bf24b60aeff4ae2acd5a356174
pwangy <blipsandclicks@gmail.com>
2024-02-06 10:26:25 -0800

    include deliverables list and notes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-// index.js
+// ! Define global variables
+const ramenAPI = 'http://localhost:3000'
 
+const menu = document.querySelector('#ramen-menu')
+const ramenForm = document.querySelector('#new-ramen')
+
+// ! 2- when item from ramen menu is clicked, display info for that item
 // Callbacks
 const handleClick = (ramen) => {
   // Add code
-};
+}
 
+// 3 - add functionality to the ramenForm
 const addSubmitListener = () => {
   // Add code
 }
 
-const displayRamens = () => {
+// ! 2- populate ramen menu
+const displayRamens = () => { //fetch here
+  // receives ramen data
+  // display img
+  // append img to #ramen-menu
+  // add event listener calling handleClick
   // Add code
-};
+}
 
-const main = () => {
+// Start logic
+// ** Attention here **: Your program should have a main() function that invokes displayRamens and addSubmitListener after the DOM has fully loaded and start the program logic.
+const main = () => { 
   // Invoke displayRamens here
   // Invoke addSubmitListener here
 }
 
 main()
+
+// ! 1 - write resuable fetch call
 
 // Export functions for testing
 export {
@@ -26,4 +41,40 @@ export {
   addSubmitListener,
   handleClick,
   main,
-};
+}
+
+
+// json-server --watch db.json (starts backend)
+// live-server (starts frontend in browser)
+
+// As you are writing your code out and completing the deliverables ensure that it works on the DOM and passes the tests by running npm test in a new terminal in the same directory
+
+// Your base URL for your API will be: http://localhost:3000
+// The endpoints you may need are:
+// GET /ramens
+// GET /ramens/:id
+// Feel free to add any additional classes or ids to any elements in the HTML file as you see fit.
+
+// ! Deliverables
+// display ramen-menu (a list of clickable images)
+// See all ramen images in the div with the id of ramen-menu. When the page loads, fire a function called displayRamens that requests the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
+// Click on an image from the #ramen-menu div and fire a callback called handleClick to see all the info about that ramen displayed inside the #ramen-detail div (where it says insert comment here and insert rating here).
+// Attach a submit even listener to the new-ramen form using a function called addSubmitListener. After the submission, create a new ramen and add it to the#ramen-menu div. The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
+
+
+// ! Advanced Deliverables
+// Note: If you are going to attempt these advanced deliverables, please be sure to have a working commit with all the Core Deliverables first!
+// User can:
+// See the details for the first ramen as soon as the page loads (without clicking on an image) <---- set a default ramen to display on page load
+// Update the rating and comment for a ramen by submitting a form. Changes should be reflected on the frontend. No need to persist. You can add this HTML to the index.html file to create the edit form (see img)
+// Delete a ramen (you can add a "delete" button if you'd like, or use an existing element to handle the delete action). The ramen should be removed from the ramen-menu div, and should not be displayed in the ramen-detail div. No need to persist.
+
+// ! Extra Advanced Deliverables
+// You'll need these endpoints for the advanced deliverables:
+// POST /ramens
+// DELETE /ramens/:id
+// PATCH /ramens/:id
+// User can:
+// persist my updates to a ramen's rating and comment. (PATCH request)
+// persist new ramens that I create (POST request)
+// persist any ramen deletions (DELETE request)
