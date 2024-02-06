b0cfe565e09af3566b298ce10a7670f9a4926779
pwangy <blipsandclicks@gmail.com>
2024-02-06 11:27:32 -0800

    include alt-txt for each img

--- src/index.js
+++ src/index.js
@@ -20,11 +20,13 @@ const addSubmitListener = () => {
 const displayRamens = (ramenObj) => {   // receives ramen data
   console.log(ramenObj)
   const img = document.createElement('img') // create image tag
-  img.src = ramenObj.image
+  img.src = ramenObj.image // include image file
+  img.alt = ramenObj.name  // include alt text
   img.addEventListener('click', e => handleClick(cakeObj)) //add event listener
   menu.append(img) //append to menu div
 }
 
+// ! 1 - write resuable fetch call
 const getRamen = () => {
   return fetch(`${ramenAPI}/ramens`)
     .then(res => {
@@ -51,8 +53,6 @@ const main = () => {
 
 main()
 
-// ! 1 - write resuable fetch call
-
 // Export functions for testing
 export {
   displayRamens,
@@ -74,8 +74,6 @@ export {
 // Feel free to add any additional classes or ids to any elements in the HTML file as you see fit.
 
 // ! Deliverables
-// display ramen-menu (a list of clickable images)
-// See all ramen images in the div with the id of ramen-menu. When the page loads, fire a function called displayRamens that requests the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
 // Click on an image from the #ramen-menu div and fire a callback called handleClick to see all the info about that ramen displayed inside the #ramen-detail div (where it says insert comment here and insert rating here).
 // Attach a submit even listener to the new-ramen form using a function called addSubmitListener. After the submission, create a new ramen and add it to the#ramen-menu div. The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
 
